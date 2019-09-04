ad8a51988c2b4f7cccfa784e2584c3025a0a0381
Deftaudio <andrei.kudryavtsev@gmail.com>
2019-09-04 14:49:09 -0700

    Rev 1.0 spec release

---
+++ SysEx scripts/init.sh
@@ -0,0 +1,106 @@
+#program all macros to default name "macro#" from 0 to 9
+./macrodefaultnames.sh
+
+#macro 0 button programming, note push mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 00 01 01 02 01 30 7f 01
+
+#macro 0 program controller 1 from 0 to 127 all range to port 1, others to erase
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 01 01 01 01 01 30 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 02 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 03 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 05 01 01 00 01 00 00 7f 00 7f 40 7f
+
+#macro 1 button programming, note toggle mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 00 01 01 02 01 30 7f 00
+
+#macro 1 program controller 1-4 from 0 to 127 all range to ALL ports, others to erase
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 01 01 01 01 01 30 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 02 02 03 01 01 31 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 03 03 03 01 01 32 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 04 04 04 01 01 33 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 05 01 01 00 01 00 00 7f 00 7f 40 7f
+
+#macro 2 button programming, CC push mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 00 01 01 01 01 30 7f 01
+
+#macro 2 program controller 1 and 2 from 0 to 127 different ranges to 1 port, others to erase
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 01 01 01 01 01 30 00 7f 00 40 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 01 01 03 01 01 31 00 7f 41 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 01 01 03 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 05 01 01 00 01 00 00 7f 00 7f 40 7f
+
+#macro 3 button programming, CC toggle mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 00 01 01 01 01 30 7f 00
+
+#macro 3 program controller 1 and 2 from 0 to 127 on 1 and 2 ports, while Note message is sent to port 3 and the end of the range
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 01 01 01 01 01 30 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 02 02 02 01 01 31 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 03 03 03 02 01 00 00 7f 7e 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 05 01 01 00 01 00 00 7f 00 7f 40 7f
+
+#macro 4 button programming, note push mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 00 01 01 02 01 30 7f 01
+
+#macro 4 program controller 1 and 2 from 0 to 127 on 1 and 2 ports, adds a Curve on controller 2, while Program Change #64 message is sent to port 3 and the end of the range
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 01 01 01 01 01 30 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 02 02 02 01 01 31 00 7f 00 7f 7f 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 03 03 03 03 01 40 00 7f 70 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 05 01 01 00 01 00 00 7f 00 7f 40 7f
+
+#macro 5 button programming, note push mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 00 01 01 02 01 30 7f 01
+
+#macro 5 program controller 1 from 0 to 127 all range to port 1 and Merging Midi Input, others to erase
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 01 01 01 01 01 30 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 02 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 03 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 05 01 01 00 01 00 00 7f 00 7f 40 7f
+#enable merging to port 1 and change a name to "Merge"
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 64 6d 65 72 67 65 20 20 01
+
+#macro 6 button programming, note push mode
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] 00 [USBport#] [MIDIport#] [type] [channel] [number] [value] [push]
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 00 01 01 02 01 30 7f 01
+
+#macro 6 program controller 1 from 0 to 127 all range to port 1 and Merging Midi Input, others to erase
+#sendmidi dev "MacroPot Port 1" hex syx 7d [macro#] [controller# 1-5] [USBport#] [MIDIport#] [type] [channel] [number] [lowvalue] [highvalue] [mapstart] [mapend] [curve] [velocity]
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 01 01 01 01 01 30 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 02 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 03 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 05 01 01 00 01 00 00 7f 00 7f 40 7f
+#enable merging to all ports and change a name to "MergALL"
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 64 4d 65 72 67 41 6c 6c 07
+
+#macro 7 empty
+sendmidi dev "MacroPot Port 1" hex syx 7d 07 64 65 6d 70 74 79 31 20 00
+
+#macro 8 empty
+sendmidi dev "MacroPot Port 1" hex syx 7d 08 64 65 6d 70 74 79 32 20 00
+
+#macro 9 Synclavier mode can be the only controller in such macro setup
+sendmidi dev "MacroPot Port 1" hex syx 7d 09 01 01 01 05 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 09 02 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 09 03 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 09 04 01 01 00 01 00 00 7f 00 7f 40 7f
+sendmidi dev "MacroPot Port 1" hex syx 7d 09 05 01 01 00 01 00 00 7f 00 7f 40 7f
+
+
---
+++ SysEx scripts/macrodefaultnames.sh
@@ -0,0 +1,11 @@
+#program all macros to default name "macro#" from 0 to 9
+sendmidi dev "MacroPot Port 1" hex syx 7d 00 64 6d 61 63 72 6f 30 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 01 64 6d 61 63 72 6f 31 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 02 64 6d 61 63 72 6f 32 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 03 64 6d 61 63 72 6f 33 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 04 64 6d 61 63 72 6f 34 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 05 64 6d 61 63 72 6f 35 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 06 64 6d 61 63 72 6f 36 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 07 64 6d 61 63 72 6f 37 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 08 64 6d 61 63 72 6f 38 20 0 f7
+sendmidi dev "MacroPot Port 1" hex syx 7d 09 64 6d 61 63 72 6f 39 20 0 f7
